1b50e675c880b73d02f8a0eb87aa4879546ad4ce
lukahorvatic <luka@codeanywhere.com>
2017-05-23 20:57:52 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* SCRIPT.JS ======================================= */
+
+
+/* Mobile Main Nav */
+$(function() {
+	$(".main-nav__m-open").click(function() {
+		$(".main-nav__mobile-wrap").addClass("slide");
+	});
+});
+
+$(function() {
+	$(".main-nav__m-close").click(function() {
+		$(".main-nav__mobile-wrap").removeClass("slide");
+	});
+});
+/* /Mobile Main Nav */
+
+
+
+/* Mobile Article Nav */
+
+/* /Mobile Article Nav */
+
+
