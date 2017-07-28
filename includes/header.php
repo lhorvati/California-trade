0549b5f1765d76d779e87b430f077bb42138fdf3
lukahorvatic <luka@codeanywhere.com>
2017-07-28 18:55:18 +0200

    created php templates

---
+++ includes/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="hr">
+  <head>
+    <title>California - Naslovnica</title>
+    <meta charset="UTF-8">
+    <meta content="California, glass, staklo, boce, ukrasne boce, boce za vino, boce za ulje, wine bottle, oil bottle, Split, TTTS, Vetropack" name="keywords">
+    <meta content="" name="description">
+    <meta content="horvaticluka@gmail.com" name="author">
+    <meta content="width=device-width; initial-scale=1.0, maximum-scale=1.0" id="meta" name="viewport">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/query.css" rel="stylesheet">
+    <link href="css/img-slider.css" rel="stylesheet">
+    <!-- Lightbox -->
+    <link href="src/lightbox/css/lightbox.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700,900|Open+Sans|Raleway:400,500,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    <!-- HEADER SECTION ************************************** -->
+    <div class="section" id="header">
+      <div class="container">
+        <!-- MAIN NAV -->
+        <nav class="main-nav">
+          <div class="main-nav__logo">
+            <a href="https://california-trade.hr">
+              <img alt="California Trade" src="img/california-logo.svg">
+            </a>
+          </div>
+          <div class="main-nav__nav mobile-hide">
+            <ul>
+              <li>
+                <a class="active" href="index.html">Naslovnica</a>
+              </li>
+              <li>
+                <a href="articles.html">Asortiman</a>
+              </li>
+              <li>
+                <a href="about.html">O nama</a>
+              </li>
+              <li>
+                <a href="partners.html">Partneri</a>
+              </li>
+              <li>
+                <a href="contact.html">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+          <div class="main-nav__m-open desktop-hide mobile-show">
+            <svg height="100%" viewbox="0 0 28 17" width="100%" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 0c-2 1.444 0 2.889 0 2.889h26S29 1.444 27 0H1zm0 7c-2 1.444 0 2.889 0 2.889h26S29 8.444 27 7H1zm0 7c-2 1.444 0 2.889 0 2.889h26s2-1.445 0-2.889H1z" fill-rule="evenodd" fill="#BD3535"></path>
+            </svg>
+          </div>
+        </nav>
+        <div class="main-nav__mobile-wrap">
+          <div class="main-nav__m-close">
+            <svg height="100%" viewbox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+            </svg>
+          </div>
+          <div class="main-nav__mobile-nav">
+            <ul>
+              <li>
+                <a class="active" href="index.html">Naslovnica</a>
+              </li>
+              <li>
+                <a href="articles.html">Asortiman</a>
+              </li>
+              <li>
+                <a href="about.html">O nama</a>
+              </li>
+              <li>
+                <a href="partners.html">Partneri</a>
+              </li>
+              <li>
+                <a href="contact.html">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,264 @@
+<?php include("includes/header.php") ?>
+
+<!-- HERO SECTION ************************************** -->
+    <div class="section" id="hero">
+      <h1 class="hero-title">
+        We offer <span>the best</span> glass products
+        <br>
+        with highest <span>quality</span>
+      </h1>
+      <ul class="cb-slideshow">
+        <li>
+          <span>Image 01</span>
+        </li>
+        <li>
+          <span>Image 02</span>
+        </li>
+        <li>
+          <span>Image 03</span>
+        </li>
+        <li>
+          <span>Image 04</span>
+        </li>
+        <li>
+          <span>Image 05</span>
+        </li>
+        <li>
+          <span>Image 06</span>
+        </li>
+      </ul>
+    </div>
+    <!-- ARTICLE NAV SECTION ************************************** -->
+    <div class="section" id="article-bar">
+      <div class="container">
+        <nav class="article-nav mobile-hide">
+          <ul>
+            <li class="dropdown">Boce</li>
+            <ul class="dropdown-list">
+              <li>
+                <a href="articles-bottle-wine.html">Boce za vino</a>
+              </li>
+              <li>
+                <a href="articles-bottle-oil.html">Boce za ulje</a>
+              </li>
+              <li>
+                <a href="articles-bottle-hard.html">Boce za žestoka pića</a>
+              </li>
+              <li>
+                <a href="articles-bottle-decore.html">Ukrasne boce</a>
+              </li>
+            </ul>
+            <li>
+              <a href="articles-caps.html">Čepovi</a>
+            </li>
+            <li>
+              <a href="articles-zippers.html">Zatvarači</a>
+            </li>
+            <li>
+              <a href="articles-jars.html">Staklenke</a>
+            </li>
+            <li>
+              <a href="articles-pvc.html">PVC kapice</a>
+            </li>
+            <li>
+              <a href="articles-cardboard.html">Kartonska ambalaža</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="article-nav-mobile">
+          <div class="article-nav-mobile__text">
+            Asortiman Kategorije
+          </div>
+          <div class="article-nav__m-open">
+            <svg height="100%" viewbox="0 0 28 17" width="100%" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 0c-2 1.444 0 2.889 0 2.889h26S29 1.444 27 0H1zm0 7c-2 1.444 0 2.889 0 2.889h26S29 8.444 27 7H1zm0 7c-2 1.444 0 2.889 0 2.889h26s2-1.445 0-2.889H1z" fill-rule="evenodd" fill="#BD3535"></path>
+            </svg>
+          </div>
+        </div>
+        <div class="article-nav__mobile-wrap">
+          <div class="article-nav__m-close">
+            <svg height="100%" viewbox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+            </svg>
+          </div>
+          <div class="article-nav__mobile-nav">
+            <ul>
+              <li>
+                <a href="articles-bottle-wine.html">Boce za vino</a>
+              </li>
+              <li>
+                <a href="articles-bottle-oil.html">Boce za ulje</a>
+              </li>
+              <li>
+                <a href="articles-bottle-hard.html">Boce za žestoka pića</a>
+              </li>
+              <li>
+                <a href="articles-bottle-decore.html">Ukrasne boce</a>
+              </li>
+              <li>
+                <a href="articles-caps.html">Čepovi</a>
+              </li>
+              <li>
+                <a href="articles-zippers.html">Zatvarači</a>
+              </li>
+              <li>
+                <a href="articles-jars.html">Staklenke</a>
+              </li>
+              <li>
+                <a href="articles-pvc.html">PVC kapice</a>
+              </li>
+              <li>
+                <a href="articles-cardboard.html">Kartonska ambalaža</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CONTENT SECTION ************************************** -->
+    <div class="section" id="content">
+      <div class="container-jumbo">
+        <!-- Vino -->
+        <div class="section-row vino">
+          <div class="container">
+            <div class="title-block title-block--center">
+              <h1 class="title">Boce za <span>Vino</span></h1>
+            </div>
+            <p>U našem asortimanu boca za vino možemo ponuditi boce različitih zapremina, boja stakla i oblika koji mogu zadovoljiti izborom svakog vinara ili ljubitelja vina. Zapremine boca su 1000 ml i 750 ml, a boje su zelena, bijela, olive i cuvee.</p>
+          </div>
+        </div>
+        <!-- Staklo 1 -->
+        <div class="section-row glass glass--left">
+          <div class="glass__img img-vino"></div>
+          <div class="glass__box">
+            Staklo je visokovrijedan i plemeniti ambalažni materijal. Ono istovremeno čuva i štiti sadržaj. Nepropusno je, pa tvari arome i okusa ne mogu ući i izaći iz stakla. Namirnice upakirane u staklu ostaju apsolutno nepromijenjene, prirodne i svježe.
+          </div>
+        </div>
+        <!-- Ulje -->
+        <div class="section-row ulje">
+          <div class="container">
+            <div class="group group--left">
+              <div class="group__content">
+                <div class="title-block title-block--left">
+                  <h1 class="title">Boce za <span>ulje</span></h1>
+                </div>
+                <p>Kao i kod boca za vino maslinari zasigurno imaju dobar izbor boca u koja mogu pohraniti svoja kvalitetna maslinova ulja. Kako se ulje najbolje čuva u staklu, tako je i izbor boca za ulje primjeren potrebama čuvanja i pakiranja maslinovog ulja. Zapremine boca su od 250 ml do 1000 ml, a boje stakla olive i cuvee.</p>
+              </div>
+              <div class="group__image img-ulje"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Zestoko pice -->
+        <div class="section-row zestoko">
+          <div class="container">
+            <div class="group group--right">
+              <div class="group__content">
+                <div class="title-block title-block--right">
+                  <h1 class="title title--right">Boce za žestoka <span>pića</span></h1>
+                </div>
+                <p>Bilo da se radi o prošeku, travarici, orahovici, lozovači, konjaku ili vinjaku, imamo adekvatne boce za flaširanje vaših proizvoda. Boja stakla je bijela i dvije su osnovne veličine; 500 ml i 1000 ml. Ukoliko želite nekakav drugi oblik i zapreminu, pogledajte naše ukrasne boce.</p>
+              </div>
+              <div class="group__image img-zestoko"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Ukrasne boce -->
+        <div class="section-row ukrasne">
+          <div class="container">
+            <div class="title-block title-block--center">
+              <h1 class="title">Boce <span>Ukrasne</span></h1>
+            </div>
+            <p>Uistinu velik izbor ukrasnih boca ima svoju primjenu kod pohranjivanja vina, ulja ili bilo kojeg drugog pića. Ukrasne boce iz našeg asortimana zadovoljit će najprobirljivije svojim oblicima, dekoracijama. Daljnjim izborom čepova, PVC kapica raznih boja, dodavanja ukusno oblikovanih etiketa i kartonskih kutija za pakiranje boca možete stvoriti vrhunsko pripremljen prizvod za prodaju ili poklon.</p>
+          </div>
+        </div>
+        <!-- Staklo 2 -->
+        <div class="section-row glass glass--right">
+          <div class="glass__img img-ukrasne"></div>
+          <div class="glass__box">
+            Za razliku od drugih ambalažnih materijala, staklo je moguće u potpunosti reciklirati i ponovno ga upotrijebiti bezbroj puta, uz nepromijenjenu kvalitetu.
+          </div>
+        </div>
+        <!-- Staklenke -->
+        <div class="section-row staklenke">
+          <div class="container">
+            <div class="group group--right">
+              <div class="group__content">
+                <div class="title-block title-block--right">
+                  <h1 class="title">Staklenke</h1>
+                </div>
+                <p>Pokazalo se kako se i hrana najbolje čuva u staklu, pogotovo ako se radi o proizvodima namjenjenim čuvanju na duže ili kraće vrijeme. Ukiseljeno povrće, namazi, marmelade, džemovi, prirodno konzervirana riba i još mnoštvo drugih receptura naći će svoj put do stola preko naših staklenki. Staklenke su standarnog i jednostavnog oblika, od bijelog su stakla, a zapremine 210 ml, 370 ml i 720 ml.</p>
+              </div>
+              <div class="group__image img-staklenke"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Cepovi -->
+        <div class="section-row cepovi">
+          <div class="container">
+            <div class="group group--left">
+              <div class="group__content">
+                <div class="title-block title-block--left">
+                  <h1 class="title">Čepovi</h1>
+                </div>
+                <p>Svaka boca neophodno treba i svoj čep da bi se sadržaj boce sačuvao. Osim zaštitnog čep ima i dekorativnu ulogu, tako da bez obzira da li se radi o čepu od prirodnog pluta ili silikona, odaberite onaj koji će i vizualno istaknuti vaš proizvod, te svojim materijalom prikladno čuvati sadržaj boce.</p>
+              </div>
+              <div class="group__image img-cepovi"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Poklopci -->
+        <div class="section-row poklopci">
+          <div class="container">
+            <div class="group group--right">
+              <div class="group__content">
+                <div class="title-block title-block--right">
+                  <h1 class="title">Poklopci</h1>
+                </div>
+                <p>Pokopci za staklenke proizvode se u standardnim dimenzijama koje odgovaraju standardima staklenki. Njihova uloga je da dobro prijanjaju otvoru staklenke i ne dopuštaju prodiranje zraka do sadržaja staklenke. U našoj ponudi su metalni i plastični poklopci. Vi ćete se, naravno, pobrinuti za sadržaj staklenke.</p>
+              </div>
+              <div class="group__image img-poklopci"></div>
+            </div>
+          </div>
+        </div>
+        <!-- PVC kapice -->
+        <div class="section-row pvc">
+          <div class="container">
+            <div class="group group--left">
+              <div class="group__content">
+                <div class="title-block title-block--left">
+                  <h1 class="title">PVC <span>kapice</span></h1>
+                </div>
+                <p>PVC kapice se proizvode i prodaju u različitim bojama, a mogu biti i posebno izrađene prema zahtjevu kupca. Kapica ima i zaštitnu i dekorativnu ulogu. Svakako pravi način da se vaš flaširani proizvod kompletira i istakne njegova kvaliteta.</p>
+              </div>
+              <div class="group__image img-pvc"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Kartonska ambalaza -->
+        <div class="section-row ambalaza">
+          <div class="container">
+            <div class="group group--right">
+              <div class="group__content">
+                <div class="title-block title-block--right">
+                  <h1 class="title">Kartonska <span>ambalaža</span></h1>
+                </div>
+                <p>Bilo da se radi o proizvodima za prodaju ili poslovne poklone vaše boce zaslužuju adekvatnu kartonsku ambalažu koja će imati i vizualnu i zaštitnu ulogu. Možete odabrati ambalažu prema broju boca i prema njihovom obliku.</p>
+              </div>
+              <div class="group__image img-karton"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- VETROPACK SECTION ************************************** -->
+    <div class="section" id="vetropack">
+      <div class="container">
+        <h1 class="title">Vetropack</h1>
+        <span class="redline"></span>
+        <p>Vetropack Straža d.d. proizvodi, prodaje i distribuira staklenu ambalažu na tržištima Hrvatske, Slovenije, Bosne i Hercegovine, Srbije, Crne Gore, Makedonije, kao i na tržištima ostalih zemalja jugoistočne Europe. Tvornica se nalazi u Humu na Sutli.</p>
+        <a class="btn btn--white" href="http://www.vetropack.hr/hr/" target="_blank">Vetropack.hr</a>
+      </div>
+    </div>
+
+
+<?php include("includes/footer.php") ?>
